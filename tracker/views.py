fdb785c0d056af3f0d11dadb3067a53186f7e4a9
jessicamarq <jessicamaq@gmail.com>
2026-03-09 22:47:58 -0300

    Making Project forms

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
+from .forms import ProjectForm
 
+#view que lista os projetos
 def project_list(request):
   projects = Project.objects.all()
   return render(request, 'tracker/project_list.html', {'projects': projects})
+
+#view que CRIA os projetos
+def project_create(request):
+  if request.method == 'POST':
+    form = ProjectForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('project_list')
+  else:
+    form = ProjectForm()
+    return render(request, 'tracker/project_form.html', {'form': form})
