d1bdd7ac0d9bdcc803151affe666db4ab0bd2108
jessicamarq <jessicamaq@gmail.com>
2026-03-21 11:31:55 -0300

    Login, logout and route protection

--- tracker/views.py
+++ tracker/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Project, Task, TimeRegister
 from .forms import ProjectForm, TaskForm, TimeRegisterForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 #view que lista os projetos
 def project_list(request):
   projects = Project.objects.all()
   return render(request, 'tracker/project_list.html', {'projects': projects})
 
+@login_required
 #view que CRIA os projetos
 def project_create(request):
   if request.method == 'POST':
@@ -18,12 +21,14 @@ def project_create(request):
   else:
     form = ProjectForm()
     return render(request, 'tracker/project_form.html', {'form': form})
-  
+
+@login_required
 #Replicando o mesmo processo dos projetos para as Tarefas
 def task_list(request):
   tasks = Task.objects.all()
   return render(request, 'tracker/task_list.html', {'tasks': tasks})
 
+@login_required
 def task_create(request):
   if request.method == 'POST':
     form = TaskForm(request.POST)
@@ -48,6 +53,7 @@ def time_create(request):
     form = TimeRegisterForm()
   return render(request, 'tracker/time_form.html', {'form': form})
 
+@login_required
 def start_timer(request, task_id):
   task = get_object_or_404(Task, id=task_id)
   TimeRegister.objects.create(
@@ -56,6 +62,7 @@ def start_timer(request, task_id):
   )
   return redirect('task_list')
 
+@login_required
 def stop_timer(request, task_id):
   active_timer = TimeRegister.objects.filter(task_id=task_id, finish__isnull=True).first()
   if active_timer:
